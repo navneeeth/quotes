e3b08acaa10e4cd84d6a3c492de61f3afb08f8a7
navneeeth <44264834+navneeeth@users.noreply.github.com>
2020-04-09 23:49:50 +0530

    General Changes: 1) Created a new dart Class named 'Quote' to store quotes and their authors. Changes in main.dart: 1) Created a List named 'quotes' of type 'Quote' Class and called the Quote constructor to store each quote and the author who said it. 2) Output the quotes from the created List using a map and toList() method that printed the quote along with the author in a Column of children text widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'quote.dart';
 void main() => runApp(MaterialApp(
   home: QuoteList(),
 ));
@@ -11,12 +11,11 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Jon Snow', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Jon Doe', text: 'The truth is rarely pure and never simple'),
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +26,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text}--${quote.author}')).toList(),
       ),
     );
   }
---
+++ lib/quote.dart
@@ -0,0 +1,10 @@
+class Quote {
+
+  String text;
+  String author;
+
+  Quote({ this.text, this.author });
+
+}
+
+//Quote myquote = Quote(text: "This is the quote text", author: "Oscar Wilde");
